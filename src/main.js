126968635b513b8143f69e23e09dd2be5503b2da
emashema <jovanovikj.emilija@gmail.com>
2024-05-08 10:57:19 +0200

    [feature] bootstrap and sass added to package.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Bootstrap and BootstrapVue
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+// Custom CSS
+import style from './assets/scss/main.scss'
+Vue.use(style)
+
+// Custom toast prototype
+Vue.prototype.$toast = function(message) {
+  this.$bvToast.toast(message, {
+    toaster: 'b-toaster-top-center',
+    toastClass: 'custom-toast',
+    autoHideDelay: 1000,
+    noCloseButton: true,
+    solid: true
+  })
+}
+
 Vue.config.productionTip = false
 
 new Vue({
